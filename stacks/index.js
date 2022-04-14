ca2befb596432e89e7ac32e845009fb9e970b0c3
radix2121 <radixdt.2121@gmail.com>
2022-04-14 11:49:26 +0530

    Adding a storage stack

--- stacks/index.js
+++ stacks/index.js
@@ -1,12 +1,19 @@
-import MyStack from "./MyStack";
+/**initial index pointing to initial myStack */
+// import MyStack from "./MyStack";
+
+// export default function main(app) {
+//   // Set default runtime for all functions
+//   app.setDefaultFunctionProps({
+//     runtime: "nodejs14.x"
+//   });
+
+//   new MyStack(app, "my-stack");
+
+//   // Add more stacks
+// }
+
+import StorageStack from "./StorageStack";
 
 export default function main(app) {
-  // Set default runtime for all functions
-  app.setDefaultFunctionProps({
-    runtime: "nodejs14.x"
-  });
-
-  new MyStack(app, "my-stack");
-
-  // Add more stacks
+  new StorageStack(app, "storage");
 }
